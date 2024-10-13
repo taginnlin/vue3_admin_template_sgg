6d3a9be43a4638759e704cefc3ba956e70cb08a2
taginnlin <2420891901@qq.com>
2024-10-13 22:25:49 +0800

    chore: 集成element-plus

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,10 @@
 <template>
   <div>
-    <el-button>Default</el-button>
-    <el-button type="primary" :icon="Search">Primary</el-button>
-    <el-button :icon="Search" circle />
-
-    <el-pagination
-      :page-size="100"
-      layout="total, prev, pager, next"
-      :total="1000"
-    />
+    <h1>我是App组件</h1>
   </div>
 </template>
 
 <script setup lang="ts">
-import { Search } from '@element-plus/icons-vue';
 </script>
 
 <style scoped></style>
