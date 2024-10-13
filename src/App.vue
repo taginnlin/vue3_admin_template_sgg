7143993a0b98993de85e462c5b3e6ffe812af2b3
taginnlin <2420891901@qq.com>
2024-10-13 22:45:18 +0800

    feat: 配置路径别名、配置提交elint修正

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
     <h1>我是App组件</h1>
+    <Test/>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Test from '@/components/Test.vue';
+</script>
 
 <style scoped></style>
