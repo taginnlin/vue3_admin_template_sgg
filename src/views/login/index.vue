a8d49b9c71e820f82d5eddcf521492430c70f64a
taginnlin <2420891901@qq.com>
2024-10-17 21:35:11 +0800

    fix: 登录表单校验规则

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,16 +3,21 @@
     <el-row>
       <el-col :span="12" :xs="0"></el-col>
       <el-col :span="12" :xs="24">
-        <el-form class="login_form">
+        <el-form
+          ref="loginFormRef"
+          class="login_form"
+          :rules="rules"
+          :model="loginData"
+        >
           <h1>Hello</h1>
           <h2>欢迎来到硅谷甄选管理平台</h2>
-          <el-form-item>
+          <el-form-item prop="username">
             <el-input
               v-model="loginData.username"
               :prefix-icon="User"
             ></el-input>
           </el-form-item>
-          <el-form-item>
+          <el-form-item prop="password">
             <el-input
               v-model="loginData.password"
               type="password"
@@ -40,13 +45,14 @@
 import { reactive, ref } from 'vue'
 import { User, Lock } from '@element-plus/icons-vue'
 import { useUserStore } from '@/store'
-import { ElNotification } from 'element-plus'
+import { ElNotification, FormInstance } from 'element-plus'
 import { useRouter } from 'vue-router'
 import { getTime } from '@/utils/time'
 
 const userStore = useUserStore()
 const router = useRouter()
 
+const loginFormRef = ref<FormInstance>()
 const loginData = reactive({
   username: 'admin',
   password: '111111',
@@ -54,25 +60,54 @@ const loginData = reactive({
 
 const loading = ref(false)
 
+const validateUsername = (_rule: any, value: any, callback: any) => {
+  if (!value) {
+    callback(new Error('用户名不能为空'))
+  } else if (value.length < 5) {
+    callback(new Error('用户名长度至少五位'))
+  } else {
+    callback()
+  }
+}
+
+const validatePassword = (_rule: any, value: any, callback: any) => {
+  if (!value) {
+    callback(new Error('密码不能为空'))
+  } else if (value.length < 6) {
+    callback(new Error('密码长度至少为六位'))
+  } else {
+    callback()
+  }
+}
+
+const rules = {
+  username: [{ validator: validateUsername, trigger: ['blur', 'change'] }],
+  password: [{ validator: validatePassword, trigger: ['blur', 'change'] }],
+}
+
 // 登录
 const loginClick = async () => {
-  loading.value = true
-  try {
-    await userStore.login(loginData)
-    ElNotification({
-      type: 'success',
-      message: '欢迎回来',
-      title: `hi, ${getTime()}好`,
-    })
-    router.push('/')
-    loading.value = false
-  } catch (error) {
-    ElNotification({
-      type: 'error',
-      message: (error as Error).message,
-    })
-    loading.value = false
-  }
+  await loginFormRef.value?.validate(async (valid) => {
+    if (valid) {
+      loading.value = true
+      try {
+        await userStore.login(loginData)
+        ElNotification({
+          type: 'success',
+          message: '欢迎回来',
+          title: `hi, ${getTime()}好`,
+        })
+        router.push('/')
+        loading.value = false
+      } catch (error) {
+        ElNotification({
+          type: 'error',
+          message: (error as Error).message,
+        })
+        loading.value = false
+      }
+    }
+  })
 }
 </script>
 
